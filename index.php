1253f8d2a379275ee7f505b6fe466a4a59345387
Frederik V. <103321581+PeaceErik@users.noreply.github.com>
2024-05-06 15:43:04 +0200

    Getting assets and header structure in place.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Brandstorm Studio - Design & Development studio in Aarhus</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/x-icon" href="assets\bs-favicon-150x150.png">
+</head>
+
+<body>
+
+    <?php include 'parts/header.php' ?>
+
+    <section class="hero">
+        
+    </section>
+    
+</body>
+
+</html>
---
+++ parts/header.php
@@ -0,0 +1,7 @@
+
+
+<?= "<header>" ?>
+    <?= "<div class='txtLogo-div'><h1>test</h1></div>" ?>
+    <?= "<div class='shapeLogo-div'></div>" ?>
+    <?= "<div class='menu-icon-div'></div>" ?>
+<?= "</header>" ?>
