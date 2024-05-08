89fdb19ab27759e6a6f74ece0138d97b7f927527
Frederik V. <103321581+PeaceErik@users.noreply.github.com>
2024-05-08 15:43:49 +0200

    Hero almost done + compressed video version, and header structure done.

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
     <title>Brandstorm Studio - Design & Development studio in Aarhus</title>
     <link rel="stylesheet" href="style.css">
     <link rel="icon" type="image/x-icon" href="assets\bs-favicon-150x150.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -14,7 +17,21 @@
     <?php include 'parts/header.php' ?>
 
     <section class="hero">
+
+        <h1 class="hero-logo">BRANDSTORM.<br>STUDIO</h1>
+
+        <video id="hero-video" src="./assets/brandstorm_bg_video_30fps.mp4" autoplay loop mute></video>
+
+        <h2 class="hero-txt">
+            BRANDSTORM.STUDIO IS A DIGITAL DESIGN STUDIO THAT<br>
+            CREATES UNIQUE DIGITAL SOLUTIONS FOR BUSINESSES<BR>
+            WHO WANT TO STAND OUT.
+        </h2>
         
+    </section>
+
+    <section class="our-approach">
+
     </section>
     
 </body>
--- parts/header.php
+++ parts/header.php
@@ -1,7 +1,18 @@
 
 
-<?= "<header>" ?>
-    <?= "<div class='txtLogo-div'><h1>test</h1></div>" ?>
-    <?= "<div class='shapeLogo-div'></div>" ?>
-    <?= "<div class='menu-icon-div'></div>" ?>
-<?= "</header>" ?>
+<?= '<header>' ?>
+
+    <?= '<div class="header-left">
+            <img class="header-logo" src="./assets/brandstorm-logo-white.svg" alt="Brandstorm Studios logo">
+        </div>' ?>
+
+    <?= '<div class="header-mid">
+            <img class="header-logo" src="./assets/transp_favicon.svg">
+         </div>' ?>
+
+    <?= '<div class="header-right">
+            <div>test</div>
+            <div></div>
+         </div>' ?>
+
+<?= '</header>' ?>
